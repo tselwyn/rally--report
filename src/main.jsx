43ad6fb7d0e34c191a896667836f486ca5d6969c
Tyler Selwyn <163087801+tselwyn@users.noreply.github.com>
2026-06-19 19:21:28 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 // ---------- ROSTER ----------
 const ROSTER = [
